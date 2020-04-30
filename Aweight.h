07d5691b315cc7bb5e3d658bc3b7dfbdafcf1732
Matuz Bence <matuzb97@gmail.com>
2020-04-30 21:15:52 +0200

    Aw

---
+++ Aweight.h
@@ -0,0 +1,52 @@
+#include<stdio.h>	//Pre-processor directive
+
+
+int AweightC (int a){
+int w;
+
+w= a;
+
+
+
+
+return w;
+}
+int AweightH (int a){
+int w;
+
+w= a*1;
+
+return w;
+}
+int AweightO (int a){
+int w;
+
+w= a*16;
+
+
+return w;
+}
+
+
+int AweightN (int a){
+int w;
+
+	w= a*14;
+
+
+return w;
+}
+int AweightS (int a){
+int w;
+
+w= a*12;
+
+
+
+
+return w;
+}
+
+
+
+
