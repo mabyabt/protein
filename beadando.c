22b7771b39e25c08879fd39e1d5205a0a926cf8b
Matuz Bence <matuzb97@gmail.com>
2020-05-21 14:43:54 +0200

    working read and output

--- beadando.c
+++ beadando.c
@@ -1,13 +1,51 @@
+
+
 #include<stdio.h>	//Pre-processor directive
 
+#include "struct.h"
 
-#include "read.h"
+
 
 void main()		//main function declaration
 {
-	read();
+	
+	    FILE *out = fopen("eredmeny.txt", "w");
+	freopen("aminosav.txt", "r", stdin);
+	
+	struct aminosav sav[100];
+	//char sav[128] ;
+	int i=1;
+	int v=0;
+	
+	while(i!=128){
+		/*scanf("%s[^\n]" , &sav[i].name);
+		printf("::%s\n", sav[i].name);*/
+		scanf("%s[^\n]" , &sav[i].name);
+		scanf("%d[^\n]" , &sav[i].c);
+		scanf("%d[^\n]" , &sav[i].h);
+		scanf("%d[^\n]" , &sav[i].n);
+		scanf("%d[^\n]" , &sav[i].s);
+		scanf("%d[^\n]" , &sav[i].o);
+		v = (sav[i].c*12+sav[i].h+sav[i].o*16+sav[i].n*14+sav[i].s*32)-14;
+		if(i%2==0 && v < 1000 && v >= 0 ){
+			v = (sav[i].c*12+sav[i].h+sav[i].o*16+sav[i].n*14+sav[i].s*32)-14;
+		fprintf(out ," %s :%d\n" ,sav[i].name,  v);
+			
+		}
+		
+		 i++;
+		
+		
+	}
+	
+//	proc(s);
+		 
+	
+	 
 
+	
+	 
+	
+	return 0;
  
- 
- return 0;
 }
---
+++ struct.h
@@ -0,0 +1,18 @@
+#include<stdio.h>	//Pre-processor directive
+  
+  
+  
+  struct aminosav{
+		char name[3];
+		char rovid;
+			
+			int c;
+			int h;
+			int n; 
+			int s;
+			int o;
+			
+			
+		
+
+  };
